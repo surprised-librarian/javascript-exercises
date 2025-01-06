c7d047f381c427f81e5f6f63ee8feceb9349d7d4
Alex Roper <alex@esrl.org>
2025-01-06 13:30:04 -0500

    Completed exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,33 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    let sum;
+    let end;
+    let i;
+    if (int1 < 0 || int2 < 0 || Number.isInteger(int1) == false || Number.isInteger(int2) == false) {
+        return 'ERROR';
+    } else {
+        if (int2 > int1) {
+            sum = int1;
+            end = int2;
+            i = int1;
+        }   else {
+            sum = int2;
+            end = int1;
+            i = int2;
+        }
+        while (i < end) {
+            i++;
+            sum += i;
+        }
+        return sum
+    }
 };
 
+sumAll(1, 4);
+sumAll(1, 4000);
+sumAll(123, 1);
+sumAll(-10, 4);
+sumAll(2.5, 4);
+sumAll(10, "90");
+sumAll(10, [90, 1]);
 // Do not edit below this line
 module.exports = sumAll;
