c9262329a0108d16122e246a6e1118cb81232fbb
Alex Roper <alex@esrl.org>
2025-01-12 13:03:25 -0500

    Completed 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let str1 = str.toLowerCase();
+    let str2 = removePunctuation(str1);
+    let str3 = str1;
+    function removePunctuation(text) {
+        return text.split('').filter(char => /[a-zA-Z0-9]/.test(char)).join('');
+    }
+    let str4 = removePunctuation(str2);
+    str5 = str4.split('').reverse().join('');
 
-};
+     if (str2 === str5) {
+        return true;
+    } else {
+        return false;
+    }
+}; 
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
+
